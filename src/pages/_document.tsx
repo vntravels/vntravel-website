650b4e6f5b7c6c39a9900e3f8df882512ab56d01
lampt <lam.pt@zinza.com.vn>
2022-06-29 23:28:12 +0700

    Initial project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name='description' content='VnTravel Website' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <link rel='icon' href='/favicon.ico' />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins&display=swap' rel='stylesheet' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../layouts';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </Layout>
+  );
+};
+
+export default Home;
