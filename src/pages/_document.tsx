d3564a69a99c02419e994c7fedf0838ee03cd1f0
lampt <lam.pt@zinza.com.vn>
2022-06-30 16:57:15 +0700

    add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta name='description' content='VnTravel Website' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='icon' href='/favicon.ico' />
-        <link href='https://fonts.googleapis.com/css2?family=Poppins&display=swap' rel='stylesheet' />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:300,400,500,700&display=swap' rel='stylesheet' />
       </Head>
       <body>
         <Main />
