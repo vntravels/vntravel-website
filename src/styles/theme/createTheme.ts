3c3a8a128fd5297bc8f95cc639c6c9d3d1d8ef82
lampt <lam.pt@zinza.com.vn>
2022-07-01 14:45:06 +0700

    Add font

--- src/styles/theme/createTheme.ts
+++ src/styles/theme/createTheme.ts
@@ -1,12 +1,37 @@
 import { createTheme } from '@mui/material/styles';
 
+const fontFamily = ['Poppins', 'sans-serif'].join(',');
+
 const customTheme = createTheme({
   palette: {
     mode: 'light',
   },
   typography: {
+    fontFamily,
+    fontWeightMedium: 400, // although Material UI's default value is 500, the text styles on the Figma design use 400
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 400,
+    },
+    h5: {},
+    h6: {},
+    subtitle1: {},
+    subtitle2: {
+      fontWeight: 400,
+    },
     body1: {
       fontWeight: 500,
+    },
+    body2: {
+      lineHeight: '20px',
+    },
+    button: {
+      boxShadow: 'none',
     },
   },
 });
