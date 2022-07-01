aab64eeba2fe3f1cf5a65c7e7cb249b8d2e7726c
lampt <lam.pt@zinza.com.vn>
2022-07-01 12:00:43 +0700

    Add emotion cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
+import { CacheProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
-import Layout from 'src/layouts';
+import createEmotionCache from 'src/utils/createEmotionCache';
+import '../styles/global.scss';
+
+const emotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <CacheProvider value={emotionCache}>
       <Component {...pageProps} />
-    </Layout>
+    </CacheProvider>
   );
 }
 
