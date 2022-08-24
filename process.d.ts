cd69f519fd64d6f06c3c81d9c68f768a7d600c8e
Lam Pham Tung <lam.pt@zinza.com.vn>
2022-08-24 16:58:32 +0700

    Add env auth

---
+++ process.d.ts
@@ -0,0 +1,15 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    API_ENDPOINT_URL: string
+    FACEBOOK_ID: string
+    FACEBOOK_SECRET: string
+    TWITTER_ID: string
+    TWITTER_SECRET: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
+    GOOGLE_AUTH_URL: string
+    AUTH0_ID: string
+    AUTH0_SECRET: string
+    AUTH0_URL: string
+  }
+}
