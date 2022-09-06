17caf260a1bb07b40bca40b39ab650e5a059b950
Lam Pham Tung <lam.pt@zinza.com.vn>
2022-09-06 13:59:20 +0700

    Add best place section

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,84 @@
+import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import React from 'react';
+
+const useStyles = makeStyles(() => ({
+  Root: {
+    padding: 24,
+    border: '1px solid #D6D2D2',
+    boxShadow: 'none',
+    borderRadius: 10,
+  },
+
+  CardMedia: {
+    borderRadius: 10,
+  },
+
+  CardContent: {
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: '0 !important',
+  },
+
+  CardTitle: {
+    fontWeight: 700,
+    fontSize: 20,
+    lineHeight: '36px',
+    color: '#132150',
+  },
+
+  CardDescription: {
+    fontSize: 16,
+    color: '#5C5B5B',
+  },
+}));
+
+type VTCardProps = {
+  title?: string;
+  description?: string;
+  image?: string;
+};
+
+const VTCard = ({ title, description, image }: VTCardProps) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.Root}>
+      {image && (
+        <CardMedia
+          className={classes.CardMedia}
+          component="img"
+          width={384}
+          height={324}
+          image={image}
+          alt={title}
+        />
+      )}
+
+      <CardContent className={classes.CardContent}>
+        {title && (
+          <Typography
+            className={classes.CardTitle}
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+          >
+            {title}
+          </Typography>
+        )}
+
+        {description && (
+          <Typography
+            className={classes.CardDescription}
+            variant="body2"
+            color="text.secondary"
+          >
+            {description}
+          </Typography>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default VTCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,11 @@ import Head from 'next/head';
 import Banner from '@/components/Banner';
 import Booking from '@/components/Booking';
 import Layout from '@/layouts';
+import SectionTitle from '@/components/Section/SectionTitle';
+import SectionContainer from '@/components/Section/SectionContainer';
+import { Grid } from '@mui/material';
+import VTCard from '@/components/Card';
+import SectionContent from '@/components/Section/SectionContent';
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +18,37 @@ const Home: NextPage = () => {
       </Head>
       <Banner />
       <Booking />
+      <SectionContainer>
+        <SectionTitle
+          title="Find a best place in Viet Nam"
+          description="Whether you’ve looking for places for a vacation. We are here to Guide you about the details you need to check in"
+        />
+        <SectionContent>
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <VTCard
+                title="Lizard"
+                description="Test"
+                image="images/hanoi.png"
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <VTCard
+                title="Lizard"
+                description="Test"
+                image="images/hanoi.png"
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <VTCard
+                title="Lizard"
+                description="Test"
+                image="images/hanoi.png"
+              />
+            </Grid>
+          </Grid>
+        </SectionContent>
+      </SectionContainer>
     </Layout>
   );
 };
